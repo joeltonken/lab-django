925060afe5d8db7173da45a5689712e25acaaa33
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-04 15:42:39 -0300

    rota de solicitacao de exames

--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def solicitar_exames(request):
+    if request.method == "GET":
+        return render(request, 'solicitar_exames.html')
