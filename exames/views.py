54751237924ce446a2b36e07ee3d6813a0f290d4
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-04 16:17:00 -0300

    pagina de solicitar exames e suas configs

--- exames/views.py
+++ exames/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import TiposExames
 
 @login_required
 def solicitar_exames(request):
+    tipos_exames = TiposExames.objects.all()
     if request.method == "GET":
-        return render(request, 'solicitar_exames.html')
+        return render(request, 'solicitar_exames.html', {'tipos_exames': tipos_exames})
