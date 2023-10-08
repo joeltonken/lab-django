ed9f2167c59618c1d0e335e5015d5cc6dd2dd5f3
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-08 17:06:54 -0300

    tela de senha e gerenciamento de pdf

--- empresarial/views.py
+++ empresarial/views.py
@@ -5,6 +5,7 @@ from django.db.models.functions import Concat
 from django.contrib.admin.views.decorators import staff_member_required
 from exames.models import SolicitacaoExame
 from django.http import HttpResponse, FileResponse
+from .utils import gerar_pdf_exames, gerar_senha_aleatoria
 
 @staff_member_required 
 def gerenciar_clientes(request):
@@ -41,3 +42,16 @@ def proxy_pdf(request, exame_id):
 
     response = exame.resultado.open()
     return FileResponse(response)
+
+@staff_member_required 
+def gerar_senha(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    if exame.senha:
+        # Baixar o documento da senha já existente
+        return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
+    
+    senha = gerar_senha_aleatoria(9)
+    exame.senha = senha
+    exame.save()
+    return FileResponse(gerar_pdf_exames(exame.exame.nome, exame.usuario, exame.senha), filename="token.pdf")
