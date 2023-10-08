1706bc16fe4616060eb6ebd10a7bb5d647486f04
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-08 16:16:20 -0300

    configurando visualizacao do pdf

--- empresarial/views.py
+++ empresarial/views.py
@@ -4,6 +4,7 @@ from django.db.models import Value
 from django.db.models.functions import Concat
 from django.contrib.admin.views.decorators import staff_member_required
 from exames.models import SolicitacaoExame
+from django.http import HttpResponse, FileResponse
 
 @staff_member_required 
 def gerenciar_clientes(request):
@@ -33,3 +34,10 @@ def cliente(request, cliente_id):
 def exame_cliente(request, exame_id):
     exame = SolicitacaoExame.objects.get(id=exame_id)
     return render(request, 'exame_cliente.html', {'exame': exame})
+
+@staff_member_required 
+def proxy_pdf(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id=exame_id)
+
+    response = exame.resultado.open()
+    return FileResponse(response)
