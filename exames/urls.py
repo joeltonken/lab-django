52731d9a7879bc2fd2b69313351eea187b5dd3f5
Joelton <114880776+joeltonken@users.noreply.github.com>
2023-10-07 22:57:56 -0300

    pagina e view do acesso do medico

--- exames/urls.py
+++ exames/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('gerenciar_exames/', views.gerenciar_exames, name="gerenciar_exames"),
     path('permitir_abrir_exame/<int:exame_id>', views.permitir_abrir_exame, name="permitir_abrir_exame"),
     path('solicitar_senha_exame/<int:exame_id>', views.solicitar_senha_exame, name="solicitar_senha_exame"),
+    path('gerar_acesso_medico/', views.gerar_acesso_medico, name="gerar_acesso_medico"),
 ]
